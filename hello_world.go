70358f0dfb593c06367eb5a1afe180b302d8d2ea
Shiguemori <shiguemori@hotmail.com>
2021-08-28 17:58:55 -0300

    Initial commit

---
+++ hello_world.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const portugueseHelloPrefix = "Ola, "
+const defaultName = "World"
+
+// This functions defines the language used to do the greetings.
+// It receives a string that can be (Portuguese, Spanish and English).
+// If something is sent different of those options the greetings will be in english
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case "Portuguese":
+		prefix = portugueseHelloPrefix
+	case "Spanish":
+		prefix = spanishHelloPrefix
+	case "English":
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
+// Hello This functions greetings according to the
+// parameters that receives (name string, language string)
+// if none of those parameters was defined the return will be "Hello, World"
+func Hello(name, language string) string {
+	if name == "" {
+		name = defaultName
+	}
+	return greetingPrefix(language) + name
+}
+
+func main() {
+	fmt.Println(Hello("Shiguemori", ""))
+}
